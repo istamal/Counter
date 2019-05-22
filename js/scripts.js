35d2cc954ecc98a2ff3344dc2b4b223dc6133410
Istamal <dagalaev@gmail.com>
2019-05-22 16:58:19 +0300

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+const button = document.getElementById('button');
+
+let firstInput = document.getElementById("input-1"),
+    secondInput = document.getElementById('input-2');
+
+button.addEventListener('click', () => {
+
+    let result = Number(firstInput.value) + Number(secondInput.value);
+    alert("Если сложить " + firstInput.value + " и " + secondInput.value + " плучается " + result.toFixed(1));
+
+});
